90763c7eb1b23ddf42de13dd777523e3caba0d0c
Bapi Gupta <rajat310198@outlook.com>
2021-04-23 01:18:23 +0530

    base model

---
+++ globals/models/base_model.py
@@ -0,0 +1,22 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """Base Model for project All models should inherit this model."""
+
+    id = models.UUIDField(
+        primary_key=True,
+        unique=True,
+        db_index=True,
+        editable=False,
+        default=uuid.uuid4,
+    )
+    created_by = models.UUIDField(null=True, blank=True)
+    updated_by = models.UUIDField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        abstract = True
