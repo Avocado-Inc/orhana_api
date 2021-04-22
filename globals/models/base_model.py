8ec05a519a6d9642427a51e92a67349bd811ba03
Bapi Gupta <rajat310198@outlook.com>
2021-04-23 01:39:55 +0530

    base dto | base response | base db response

--- globals/models/base_model.py
+++ globals/models/base_model.py
@@ -17,6 +17,7 @@ class BaseModel(models.Model):
     updated_by = models.UUIDField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
+    is_active = models.BooleanField(default=True, db_index=True)
 
     class Meta:
         abstract = True
